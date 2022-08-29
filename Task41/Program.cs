f8a50908c4880150f39a6ad0d7640b3ad9fe3cf3
AlinaKov <alkovkova@gmail.com>
2022-08-30 00:01:17 +0500

    Added Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+using System;
+
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int count = baseArray.Length;
+int index = 0;
+int position = 0;
+while (index < count)
+{
+    if (baseArray[index] > 0)
+    {
+        position++;
+    }
+    index++;
+}
+
+Console.Write("Количество положительных чисел: " + position);
+
